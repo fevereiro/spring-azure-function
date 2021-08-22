6c33663f6283c64001448235aac1abd746b570a2
Denis Alberto Santana Fevereiro <denis.alberto@totvs.com.br>
2021-08-22 20:05:02 -0300

    feat: add param validation

--- src/main/java/com/bankholidays/BankHolidayHandler.java
+++ src/main/java/com/bankholidays/BankHolidayHandler.java
@@ -1,6 +1,7 @@
 package com.bankholidays;
 
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 import com.bankholidays.dto.ResponseDTO;
 import com.bankholidays.services.BankHolidayService;
@@ -14,6 +15,8 @@ import org.springframework.cloud.function.adapter.azure.FunctionInvoker;
 
 public class BankHolidayHandler extends FunctionInvoker<String, ResponseDTO> {
 
+        private static final int YEAR_LENGTH = 4;
+
         @Autowired
         BankHolidayService bankHolidayService;
 
@@ -22,18 +25,24 @@ public class BankHolidayHandler extends FunctionInvoker<String, ResponseDTO> {
                         HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
                         ExecutionContext context) {
 
-                String year = request.getQueryParameters().getOrDefault("year", "2021");
-                ResponseDTO response = handleRequest(year, context);
+                String year = request.getQueryParameters().get("year");
+                if (!this.isValidYear(year)) {
+                        return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Invalid year param").build();
+                }
 
+                ResponseDTO response = handleRequest(year, context);
                 if (response.hasError()) {
                         return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR)
                                         .body(response.getErrorMessage()).build();
                 }
-
                 return request.createResponseBuilder(HttpStatus.OK).body(response.getFileOutput())
                                 .header("Content-Disposition",
-                                                "attachment; filename=england-wales-bank-holidays" + year + ".csv")
+                                                "attachment; filename=england-wales-bank-holidays-" + year + ".csv")
                                 .header("Content-Type", "text/csv").build();
 
+        }
+
+        private boolean isValidYear(String year) {
+                return (year != null && !Pattern.matches("[a-zA-Z]+", year) && year.length() == YEAR_LENGTH);
         }
 }
